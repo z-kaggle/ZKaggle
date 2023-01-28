7751e6869d1445d42e0a2ab560afd8b33d5bf17d
Madhur Gupta <madhurgupta107@gmail.com>
2023-01-28 23:19:49 +0530

    Add create bounty steps UI

--- ui/components/CreateBounty/ProcessingStep.tsx
+++ ui/components/CreateBounty/ProcessingStep.tsx
@@ -7,6 +7,7 @@ import {
   ListItemText,
   Button,
   Divider,
+  Stack,
 } from "@mui/material";
 
 import PaidIcon from "@mui/icons-material/Paid";
@@ -16,18 +17,56 @@ type ProcessingStepProps = {
   goToPreviousStep: () => void;
 };
 
-const ProcessingStep = ({ goToNextStep, goToPreviousStep }: ProcessingStepProps) => {
+const ProcessingStep = ({
+  goToNextStep,
+  goToPreviousStep,
+}: ProcessingStepProps) => {
   return (
     <div
       css={css`
         display: flex;
         flex-direction: column;
-        max-width: 50%;
+        max-width: 70%;
         min-width: 300px;
       `}
     >
-      <h1>Your task is waiting for picking up!</h1>
-      <h5>Your task will be shown in the task pool.</h5>
+      <h1>Your task is being handling by a bounty hunter!</h1>
+      <h5>It will take up to 2-3 days.</h5>
+
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          max-width: 35vw;
+          min-width: 300px;
+        `}
+      >
+        <Stack paddingTop={6} spacing={0}>
+          <Stack direction="row" spacing={10} justifyContent="space-between">
+            <h2 style={{ padding: "0", margin: "0" }}>MNIST Training Task</h2>
+            <Button
+              variant="outlined"
+              sx={{
+                width: "150px",
+                alignSelf: "flex-end",
+              }}
+            >
+              0.5 ETH
+            </Button>
+          </Stack>
+          <h5>Your task will be shown in the task pool.</h5>
+        </Stack>
+
+        <h2>Input</h2>
+        <List component="nav" aria-label="secondary mailbox folder">
+          <ListItemButton>
+            <ListItemText primary="Input 1" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemText primary="Input 2" />
+          </ListItemButton>
+        </List>
+      </div>
     </div>
   );
 };
--- ui/components/CreateBounty/PublishStep.tsx
+++ ui/components/CreateBounty/PublishStep.tsx
@@ -7,6 +7,7 @@ import {
   ListItemText,
   Button,
   Divider,
+  Stack,
 } from "@mui/material";
 
 import PaidIcon from "@mui/icons-material/Paid";
@@ -28,6 +29,41 @@ const PublishStep = ({ goToNextStep, goToPreviousStep }: PublishStepProps) => {
     >
       <h1>Your task is waiting for picking up!</h1>
       <h5>Your task will be shown in the task pool.</h5>
+
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          max-width: 35vw;
+          min-width: 300px;
+        `}
+      >
+        <Stack paddingTop={6} spacing={0}>
+          <Stack direction="row" spacing={10} justifyContent="space-between">
+            <h2 style={{ padding: "0", margin: "0" }}>MNIST Training Task</h2>
+            <Button
+              variant="outlined"
+              sx={{
+                width: "150px",
+                alignSelf: "flex-end",
+              }}
+            >
+              0.5 ETH
+            </Button>
+          </Stack>
+          <h5>Your task will be shown in the task pool.</h5>
+        </Stack>
+
+        <h2>Input</h2>
+        <List component="nav" aria-label="secondary mailbox folder">
+          <ListItemButton>
+            <ListItemText primary="Input 1" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemText primary="Input 2" />
+          </ListItemButton>
+        </List>
+      </div>
     </div>
   );
 };
