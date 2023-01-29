c2687fccb92bc7d5f6ad9a024be1a943a5975c93
Cathie So <socathie@gmail.com>
2023-01-30 02:09:08 +0800

    feat: `BountyFactory` contract and tests

--- hardhat/contracts/BountyFactory.sol
+++ hardhat/contracts/BountyFactory.sol
@@ -3,8 +3,31 @@
 pragma solidity ^0.8.17;
 
 import "./Bounty.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract BountyFactory {
     // TODO: deploy bounty contract, passing msg.sender as an argument, with the address recorded AND returned
     // Ref: https://blog.logrocket.com/cloning-solidity-smart-contracts-factory-pattern/
+
+    address public immutable bountyTemplate;
+    address[] public bounties;
+
+    event BountyCreated(address indexed bounty);
+
+    constructor() {
+        bountyTemplate = address(new Bounty());
+    }
+
+    function createBounty(
+        string memory _name,
+        string memory _description,
+        bytes memory _dataCID
+    ) public payable returns (address) {
+        require(msg.value > 0, "BountyFactory: must send more than 0 wei to create bounty");
+        address clone = Clones.clone(bountyTemplate);
+        Bounty(clone).initialize{value: msg.value}(msg.sender, _name, _description, _dataCID);
+        bounties.push(clone);
+        emit BountyCreated(clone);
+        return clone;
+    }
 }
--- hardhat/test/factory.test.js
+++ hardhat/test/factory.test.js
@@ -2,5 +2,45 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("BountyFactory test", function () {
-    // TODO: Add tests for the bounty factory contract
+    let factory;
+    let Bounty;
+
+    before(async function () {
+        const BountyFactory = await ethers.getContractFactory("BountyFactory");
+        factory = await BountyFactory.deploy();
+        await factory.deployed();
+
+        Bounty = await ethers.getContractFactory("Bounty");
+    });
+
+    it("Should create a new bounty", async function () {
+        const tx = await factory.createBounty(
+            "Bounty 1",
+            "This is the first bounty",
+            0x00,
+            { value: ethers.utils.parseEther("1") }
+        );
+        await tx.wait();
+
+        const bounty = await Bounty.attach(await factory.bounties(0));
+
+        expect(await bounty.owner()).to.equal(await ethers.provider.getSigner(0).getAddress());
+        expect(await ethers.provider.getBalance(bounty.address)).to.equal(ethers.utils.parseEther("1"));
+        
+        expect(await bounty.name()).to.equal("Bounty 1");
+        expect(await bounty.description()).to.equal("This is the first bounty");
+        expect(await bounty.dataCID()).to.equal("0x00");
+    });
+
+    it("Should calculate future bounty address", async function () {
+        const nonce = await ethers.provider.getTransactionCount(factory.address);
+        const futureAddress = ethers.utils.getContractAddress({ from: factory.address, nonce: nonce });
+
+        await expect(factory.createBounty(
+            "Bounty 2",
+            "This is the second bounty",
+            0x00,
+            { value: ethers.utils.parseEther("1") }
+        )).to.emit(factory, "BountyCreated").withArgs(futureAddress);
+    });
 });
