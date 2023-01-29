46322d5aabc1e4a5f6388bf1a8765a7ac2718e19
Cathie So <socathie@gmail.com>
2023-01-30 00:51:52 +0800

    feat: Bounty contract and tests

--- hardhat/contracts/Bounty.sol
+++ hardhat/contracts/Bounty.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IVerifier.sol";
+import "hardhat/console.sol";
 
 contract Bounty is Ownable {
     // variables set by bounty provier at Tx 1 (constructor)
@@ -13,19 +14,25 @@ contract Bounty is Ownable {
     // reward amount is not stored, use contract balance instead
 
     // variables set by bounty hunter at Tx 2
+    address public bountyHunter;
     bytes public zkeyCID;
     bytes public circomCID;
     IVerifier public verifier;
     uint[2] public a;
     uint[2][2] public b;
     uint[2] public c;
-    uint public hashedInput;
+    uint[] public hashedInput;
 
     // variables set by bounty provider at Tx 3
     bool public isComplete;
 
     // variabels set by bounty hunter at Tx 4
     uint[] public input;
+
+    uint8 public constant CID_VERSION = 1;
+    uint8 public constant CID_CODEC = 0x55; // for raw buffer
+    uint8 public constant CID_HASH = 0x12; // for sha256
+    uint8 public constant CID_LENGTH = 32; // for sha256
 
     // ! current design only allows one bounty hunter to submit proof
     // TODO: allow multiple bounty hunters to submit proof
@@ -42,6 +49,7 @@ contract Bounty is Ownable {
         string memory _description,
         bytes memory _dataCID
     ) payable {
+        require(msg.value > 0, "Bounty reward must be greater than 0");
         transferOwnership(_owner);
         name = _name;
         description = _description;
@@ -61,12 +69,34 @@ contract Bounty is Ownable {
         uint[2] memory _a,
         uint[2][2] memory _b,
         uint[2] memory _c,
-        uint _hashedInput
+        uint[] memory _hashedInput
+        /*
+         * should be length 4, will extend to support arbitrary length
+         * first two elements are hash of computed results
+         * last two elements are the dataCID
+         */
     ) public {
+        require(_hashedInput.length == 4, "Invalid hashed input length"); // TODO: support arbitrary length
+        require(bountyHunter == address(0), "Bounty already submitted");
+        // verifier address should not be 0x0
+        require(_verifier != address(0), "Invalid verifier address");
+        // * can save gas by pre-hashing dataCID
+        require(
+            keccak256(dataCID) ==
+                keccak256(
+                    abi.encodePacked(
+                        CID_VERSION,
+                        CID_CODEC,
+                        CID_HASH,
+                        CID_LENGTH,
+                        concatDigest(_hashedInput[2], _hashedInput[3])
+                    )
+                ),
+            "Data CID mismatch"
+        );
+
         verifier = IVerifier(_verifier);
-        uint[] memory _input = new uint[](1);
-        _input[0] = _hashedInput;
-        require(verifier.verifyProof(_a, _b, _c, _input), "Invalid proof");
+        require(verifier.verifyProof(_a, _b, _c, _hashedInput), "Invalid proof");
         a = _a;
         b = _b;
         c = _c;
@@ -74,6 +104,8 @@ contract Bounty is Ownable {
 
         zkeyCID = _zkeyCID;
         circomCID = _circomCID;
+
+        bountyHunter = msg.sender;
     }
 
     /*
@@ -84,7 +116,7 @@ contract Bounty is Ownable {
     */
     function releaseBounty() public onlyOwner {
         require(!isComplete, "Bounty is already complete");
-        require(hashedInput != 0, "Bounty hunter has not submitted proof");
+        require(a[0] != 0, "Bounty hunter has not submitted proof");
         isComplete = true;
     }
 
@@ -95,13 +127,36 @@ contract Bounty is Ownable {
         * only callable if bounty is complete
     */
     function claimBounty(uint[] memory _input) public {
+        require(_input.length == 2, "Invalid input length"); // TODO: support arbitrary length
+        require(msg.sender == bountyHunter, "Only bounty hunter can claim bounty");
         require(isComplete, "Bounty is not complete");
-        require(sha256(abi.encode(_input)) == bytes32(hashedInput), "Invalid preimage");
+        require(address(this).balance > 0, "Bounty already claimed");
+
+        // check if preimage matches hashed input
+        require(
+            sha256(abi.encode(_input[0], _input[1])) == concatDigest(hashedInput[0], hashedInput[1]),
+            "Invalid preimage"
+        );
         input = _input;
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    
+    // function to concat input into digest
+    function concatDigest(uint input1, uint input2) public pure returns (bytes32) {
+        return bytes32((input1 << 128) + input2);
+    }
+
+    // view function to verify proof
+    function verifyProof(
+        uint[2] memory _a,
+        uint[2][2] memory _b,
+        uint[2] memory _c,
+        uint[] memory _hashedInput
+    ) public view returns (bool) {
+        require(verifier != IVerifier(address(0)), "Verifier not set");
+        return verifier.verifyProof(_a, _b, _c, _hashedInput);
+    }
+
     // function to cancel bounty and withdraw reward
 
     // TODO: function to edit bounty details
--- hardhat/test/bounty.test.js
+++ hardhat/test/bounty.test.js
@@ -1,6 +1,305 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { groth16 } = require("snarkjs");
+
+const fs = require("fs");
+const base32 = require("base32.js");
+
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+
+// cid of the uploaded file in base32
+const ipfs_cid = "bafkreig42jyiawthjkmskza765hn6krgqs7uk7cmtjmggb6mgnjql7dqje";
+
+const json = require("./mnist_latest_input.json");
+
+let INPUT = {};
+
+for (const [key, value] of Object.entries(json)) {
+    if (Array.isArray(value)) {
+        let tmpArray = [];
+        for (let i = 0; i < value.flat().length; i++) {
+            tmpArray.push(Fr.e(value.flat()[i]));
+        }
+        INPUT[key] = tmpArray;
+    } else {
+        INPUT[key] = Fr.e(value);
+    }
+}
 
 describe("Bounty contract test", function () {
-    // TODO: Add tests for the bounty contract
+    let cidraw; // raw CID of the uploaded file
+
+    describe("Tx 1: Creating a bounty", function () {
+
+        before(async function () {
+            const decoder = new base32.Decoder();
+            cidraw = decoder.write(ipfs_cid.slice(1)).finalize();
+            // console.log(cid);
+        });
+
+        it("Should reject deploying the contract is msg.value is 0", async function () {
+            const owner = await ethers.provider.getSigner(0).getAddress();
+            const Bounty = await ethers.getContractFactory("Bounty");
+            await expect(Bounty.deploy(
+                owner,
+                "Bounty 1",
+                "This is the first bounty",
+                cidraw
+            )).to.be.revertedWith("Bounty reward must be greater than 0");
+        });
+
+        it("Should deploy the contract and set owner to first input", async function () {
+            const owner = await ethers.provider.getSigner(0).getAddress();
+            const Bounty = await ethers.getContractFactory("Bounty");
+            const bounty = await Bounty.deploy(
+                owner,
+                "Bounty 1",
+                "This is the first bounty",
+                cidraw,
+                { value: ethers.utils.parseEther("1")}
+            );
+            expect(await bounty.name()).to.equal("Bounty 1");
+            expect(await bounty.description()).to.equal("This is the first bounty");
+            expect(await bounty.dataCID()).to.equal('0x' + cidraw.toString('hex'));
+            expect(await bounty.owner()).to.equal(owner);
+        });
+    });
+
+    let a, b, c, Input;
+
+    describe("Tx 2: Submitting a bounty", function () {
+        let bounty, verifier;
+
+        before(async function () {
+            // Deploy the contract
+            const owner = await ethers.provider.getSigner(0).getAddress();
+            const Bounty = await ethers.getContractFactory("Bounty");
+            bounty = await Bounty.deploy(
+                owner,
+                "Bounty 1",
+                "This is the first bounty",
+                cidraw,
+                { value: ethers.utils.parseEther("1")}
+            );
+            await bounty.deployed();
+
+            // Deploy the verifier
+            const Verifier = await ethers.getContractFactory("Verifier");
+            verifier = await Verifier.deploy();
+            await verifier.deployed();
+
+            // Generate proof
+
+            const bytes = fs.readFileSync("assets/mnist_image.pgm");
+            const binary = [...bytes].map((b) => b.toString(2).padStart(8, "0").split("")).flat();
+
+            INPUT["in"] = binary;
+            INPUT["salt"] = "123456789";
+
+            const { proof, publicSignals } = await groth16.fullProve(INPUT, "circuits/build/circuit_js/circuit.wasm", "circuits/build/circuit_final.zkey");
+
+            const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
+
+            const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+
+            a = [argv[0], argv[1]];
+            b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+            c = [argv[6], argv[7]];
+            Input = argv.slice(8);
+        });
+
+        it("Should reject a bounty with wrong proof", async function () {
+            await expect(bounty.connect(ethers.provider.getSigner(1)).
+                submitBounty(
+                    0x0,
+                    0x0,
+                    verifier.address,
+                    [0, 0],
+                    [[0, 0], [0, 0]],
+                    [0, 0],
+                    Input
+                )).to.be.revertedWith("Invalid proof");
+        });
+
+        it("Should reject a bounty with wrong verifier", async function () {
+            await expect(bounty.connect(ethers.provider.getSigner(1)).
+                submitBounty(
+                    0x0,
+                    0x0,
+                    ethers.constants.AddressZero,
+                    a,
+                    b,
+                    c,
+                    Input
+                )).to.be.revertedWith("Invalid verifier address");
+        });
+
+        it("Should reject a bounty with wrong input", async function () {
+            await expect(bounty.connect(ethers.provider.getSigner(1)).
+                submitBounty(
+                    0x0,
+                    0x0,
+                    verifier.address,
+                    a,
+                    b,
+                    c,
+                    [0, 0, 0, 0]
+                )).to.be.revertedWith("Data CID mismatch");
+        });
+
+
+
+        it("Should submit a bounty", async function () {
+            const submitter = await ethers.provider.getSigner(1).getAddress();
+            const tx = await bounty.connect(ethers.provider.getSigner(1)).
+                submitBounty(
+                    0x0,
+                    0x0,
+                    verifier.address,
+                    a,
+                    b,
+                    c,
+                    Input
+                );
+            await tx.wait();
+            expect(await bounty.bountyHunter()).to.equal(submitter);
+        });
+
+        it("Should reject submitting a submitted bounty", async function () {
+            await expect(bounty.connect(ethers.provider.getSigner(1)).
+                submitBounty(
+                    0x0,
+                    0x0,
+                    verifier.address,
+                    a,
+                    b,
+                    c,
+                    Input
+                )).to.be.revertedWith("Bounty already submitted");
+        });
+    });
+
+    describe("Tx 3: Releasing a bounty", function () {
+        let bounty, verifier;
+
+        before(async function () {
+            // Deploy the contract
+            const owner = await ethers.provider.getSigner(0).getAddress();
+            const Bounty = await ethers.getContractFactory("Bounty");
+            bounty = await Bounty.deploy(
+                owner,
+                "Bounty 1",
+                "This is the first bounty",
+                cidraw,
+                { value: ethers.utils.parseEther("1")}
+            );
+            await bounty.deployed();
+
+            // Deploy the verifier
+            const Verifier = await ethers.getContractFactory("Verifier");
+            verifier = await Verifier.deploy();
+            await verifier.deployed();
+        });
+
+        it("Should reject releasing an unsubmitted bounty", async function () {
+            await expect(bounty.releaseBounty()).to.be.revertedWith("Bounty hunter has not submitted proof");
+        });
+
+        it("Should reject releasing a bounty by non-owner", async function () {
+
+            const tx = await bounty.connect(ethers.provider.getSigner(1)).
+                submitBounty(
+                    0x0,
+                    0x0,
+                    verifier.address,
+                    a,
+                    b,
+                    c,
+                    Input
+                );
+            await tx.wait();
+            await expect(bounty.connect(ethers.provider.getSigner(1)).releaseBounty()).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+
+        it("Should release a bounty", async function () {
+            const tx = await bounty.releaseBounty();
+            await tx.wait();
+            expect(await bounty.isComplete()).to.equal(true);
+        });
+
+        it("Should reject releasing a bounty twice", async function () {
+            await expect(bounty.releaseBounty()).to.be.revertedWith("Bounty is already complete");
+        });
+    });
+
+    describe("Tx 4: Claiming a bounty", function () {
+        let bounty, verifier;
+
+        before(async function () {
+            // Deploy the contract
+            const owner = await ethers.provider.getSigner(0).getAddress();
+            const Bounty = await ethers.getContractFactory("Bounty");
+            bounty = await Bounty.deploy(
+                owner,
+                "Bounty 1",
+                "This is the first bounty",
+                cidraw,
+                { value: ethers.utils.parseEther("1")}
+            );
+            await bounty.deployed();
+
+            // Deploy the verifier
+            const Verifier = await ethers.getContractFactory("Verifier");
+            verifier = await Verifier.deploy();
+            await verifier.deployed();
+
+            const tx = await bounty.connect(ethers.provider.getSigner(1)).
+                submitBounty(
+                    0x0,
+                    0x0,
+                    verifier.address,
+                    a,
+                    b,
+                    c,
+                    Input
+                );
+            await tx.wait();
+        });
+
+        it("Should reject claiming a bounty by non bounty hunter", async function () {
+            await expect(bounty.connect(ethers.provider.getSigner(2))
+                .claimBounty([0, 0])).to.be.revertedWith("Only bounty hunter can claim bounty");
+        });
+
+        it("Should reject claiming an uncompleted bounty", async function () {
+            await expect(bounty.connect(ethers.provider.getSigner(1))
+                .claimBounty([0, 0]))
+                .to.be.revertedWith("Bounty is not complete");
+        });
+
+        it("Should claim a bounty", async function () {
+            const bountyHunter = await ethers.provider.getSigner(1).getAddress();
+            const balance = await ethers.provider.getBalance(bountyHunter);
+            let tx = await bounty.releaseBounty();
+            await tx.wait();
+            tx = await bounty.connect(ethers.provider.getSigner(1))
+                .claimBounty([5, 123456789]);
+            await tx.wait();
+
+            // Check that the bounty hunter has been paid
+            expect(await ethers.provider.getBalance(bountyHunter)).to.greaterThan(balance);
+            // Check that the input has been updated
+            expect(await bounty.input(0)).to.equal(5);
+            expect(await bounty.input(1)).to.equal(123456789);
+        });
+
+        it("Should reject claiming a claimed bounty", async function () {
+            await expect(bounty.connect(ethers.provider.getSigner(1))
+                .claimBounty([0, 0]))
+                .to.be.revertedWith("Bounty already claimed");
+        });
+    });
 });
